47008a7a0fad210180e739ad4840330596fcbf6a
Tanay Shankar <tanaysh7@gmail.com>
2020-04-30 12:14:05 -0700

    Procfile

--- together_in_covid/settings.py
+++ together_in_covid/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*wyc_@v3%@**@fb)qy^ba#7-m^w=d93mw@*rbnxb1ewt#51-!l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
